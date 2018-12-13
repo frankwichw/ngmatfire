b1334f88d95f822c329e334636e764015aaf8c36
frankwichw <wenona.frankwich@capco.com>
2018-12-13 10:17:07 -0500

    angular/firebase/material starter project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, /* MaterialModule */ MatRippleModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    /* MaterialModule, */
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
