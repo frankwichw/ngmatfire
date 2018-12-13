a282f05bfd28bd35cb7900b64e71519a30706dd4
frankwichw <wenona.frankwich@capco.com>
2018-12-13 14:34:56 -0500

    firebase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { dotenv } from '../dotenv/dotenv';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngmatfire';
+
+  ngOnInit() {
+    // initializing firebase
+    var config = {
+        apiKey: dotenv.FIREBASE_CONFIG.apiKey,
+        authDomain: dotenv.FIREBASE_CONFIG.authDomain,
+        databaseURL: dotenv.FIREBASE_CONFIG.databaseURL,
+        projectId: dotenv.FIREBASE_CONFIG.projectId,
+        storageBucket: dotenv.FIREBASE_CONFIG.storageBucket,
+        messagingSenderId: dotenv.FIREBASE_CONFIG.messagingSenderId
+      };
+    firebase.initializeApp(config);
+   };
 }
